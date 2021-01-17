24eeedd9cead35777571afdfae89a2a2f5e82536
Ngo Khanh <manhkhanhad@gmail.com>
2021-01-17 23:17:59 +0700

    add Network

---
+++ Network/pix2pix.py
@@ -0,0 +1,62 @@
+import torch 
+import torch.nn as nn
+
+class ResnetBlock(nn.Module):
+    """ Define a Resmet Block """
+
+    def __init__(self,dim,padding_type,use_bias,use_dropout,norm_layer):
+        super(ResnetBlock,self).__init__()
+        self.conv_block = seld.buildResnetBlock(self,dim,padding_type,use_bias,use_dropout,norm_layer)
+    
+    def buildResnetBlock(self,dim,padding_type,use_bias,use_dropout,norm_layer):
+        """ Build Block: Padding --> Conv2d --> Norm --> ReLU --> Padding --> Conv --> Norm 
+        
+        Parameters:
+            dim (int)           -- the number of channels in the conv layer.
+            padding_type (str)  -- the name of padding layer: reflect | replicate | zero
+            norm_layer          -- normalization layer
+            use_dropout (bool)  -- if use dropout layers.
+            use_bias (bool)     -- if the conv layer uses bias or not
+        Returns a conv block (with a conv layer, a normalization layer, and a non-linearity layer (ReLU))
+
+        """
+
+        conv = nn.Sequential()
+
+        zero_padding = 0
+        if padding_type == 'reflect':
+            conv.append(nn.ReflectionPad1d(1))
+        if padding_type == 'replicate':
+            conv.append(nn.ReplicationPad1d(1))
+        if padding_type == 'zero':
+            zero_padding = 1
+        else:
+            raise NotImplementedError('padding [%s] is not implemented' % padding_type)
+        
+        conv.append(nn.Conv2d(dim,dim,kernel_size=3,padding=zero_padding,bias=use_bias))
+        conv.append(norm_layer(dim))
+        conv.append(nn.ReLU(True))
+
+        if use_dropout:
+            conv.append(nn.Dropout(0.5))
+
+        zero_padding = 0
+        if padding_type == 'reflect':
+            conv.append(nn.ReflectionPad1d(1))
+        if padding_type == 'replicate':
+            conv.append(nn.ReplicationPad1d(1))
+        if padding_type == 'zero':
+            zero_padding = 1
+        else:
+            raise NotImplementedError('padding [%s] is not implemented' % padding_type)
+        
+        conv.append(nn.Conv2d(dim,dim,kernel_size=3,padding=zero_padding,bias=use_bias))
+        conv.append(norm_layer(dim))
+
+        return conv
+
+    def foward(self, x):
+        out = x + self.conv_block(x)
+        return out
+
+
