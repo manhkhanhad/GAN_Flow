1d24a53fb64f69fb2b7c8233468e1e45fcfd6985
Ngo Khanh <manhkhanhad@gmail.com>
2021-02-25 21:53:59 +0700

    add train.py

---
+++ loss.py
@@ -0,0 +1,25 @@
+class GANLoss(nn.Module):
+    """Define different GAN objectives"""
+    def __init__(self, gan_mode = 'lsgan', target_real_label = 1.0, target_fake_label = 0.0):
+        super(GANLoss,self).__init__()
+        self.register_buffer('real_label', torch.tensor(target_real_label))
+        self.register_buffer('fake_label', torch.tensor(target_fake_label))
+        self.gan_mode = gan_mode
+
+        if gan_mode == 'lsgan':
+            self.loss = nn.MSELoss()
+        else:
+            raise NotImplementedError('gan mode %s not implemented' % gan_mode)
+
+    def get_target_tensor(self, prediction, target_is_real):
+        if target_is_real:
+            target_tensor = self.real_label
+        else:
+            target_tensor = self.fake_label
+        return target_tensor.expand_as(prediction) 
+    
+    def __call__(self, prediction, target_is_real):
+        if self.gan_mode in ['lsgan', 'vanilla']:
+            target_tensor = self.get_target_tensor(prediction, target_is_real)
+            loss = self.loss(prediction, target_tensor)
+        return loss
---
+++ train.py
@@ -0,0 +1,63 @@
+import argparse
+import time
+
+if __name__= '__main__':
+    #Define the common parserions
+    parser = argparse.Argumentparser()
+    # basic parameters
+    parser.add_argument('--dataroot', required=True, help='path to images (should have subfolders trainA, trainB, valA, valB, etc)')
+    parser.add_argument('--name', type=str, default='experiment_name', help='name of the experiment. It decides where to store samples and models')
+    parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
+    parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints', help='models are saved here')
+    # model parameters
+    parser.add_argument('--model', type=str, default='pix2pix', help='chooses which model to use.')
+    parser.add_argument('--input_nc', type=int, default=3, help='# of input image channels: 3 for RGB and 1 for grayscale')
+    parser.add_argument('--output_nc', type=int, default=3, help='# of output image channels: 3 for RGB and 1 for grayscale')
+    parser.add_argument('--ngf', type=int, default=64, help='# of gen filters in the last conv layer')
+    parser.add_argument('--ndf', type=int, default=64, help='# of discrim filters in the first conv layer')
+    parser.add_argument('--netD', type=str, default='basic', help='specify discriminator architecture [basic | n_layers | pixel]. The basic model is a 70x70 PatchGAN. n_layers allows you to specify the layers in the discriminator')
+    parser.add_argument('--netG', type=str, default='resnet_9blocks', help='specify generator architecture [resnet_9blocks | resnet_6blocks | unet_256 | unet_128]')
+    parser.add_argument('--n_layers_D', type=int, default=3, help='only used if netD==n_layers')
+    parser.add_argument('--norm', type=str, default='instance', help='instance normalization or batch normalization [instance | batch | none]')
+    # dataset parameters
+    parser.add_argument('--dataset_mode', type=str, default='unaligned', help='chooses how datasets are loaded. [unaligned | aligned | single | colorization]')
+    parser.add_argument('--num_threads', default=4, type=int, help='# threads for loading data')
+    parser.add_argument('--batch_size', type=int, default=1, help='input batch size')
+    parser.add_argument('--load_size', type=int, default=286, help='scale images to this size')
+    parser.add_argument('--crop_size', type=int, default=256, help='then crop to this size')
+    # additional parameters
+    parser.add_argument('--epoch', type=str, default='latest', help='which epoch to load? set to latest to use latest cached model')
+    parser.add_argument('--load_iter', type=int, default='0', help='which iteration to load? if load_iter > 0, the code will load models by iter_[load_iter]; otherwise, the code will load models by [epoch]')
+    parser.add_argument('--verbose', action='store_true', help='if specified, print more debugging information')
+    parser.add_argument('--suffix', default='', type=str, help='customized suffix: parser.name = parser.name + suffix: e.g., {model}_{netG}_size{load_size}')
+     # network saving and loading parameters
+    parser.add_argument('--save_latest_freq', type=int, default=5000, help='frequency of saving the latest results')
+    parser.add_argument('--save_epoch_freq', type=int, default=5, help='frequency of saving checkpoints at the end of epochs')
+    parser.add_argument('--save_by_iter', action='store_true', help='whether saves model by iteration')
+    parser.add_argument('--continue_train', action='store_true', help='continue training: load the latest model')
+    parser.add_argument('--epoch_count', type=int, default=1, help='the starting epoch count, we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>, ...')
+    parser.add_argument('--phase', type=str, default='train', help='train, val, test, etc')
+    # training parameters
+    parser.add_argument('--n_epochs', type=int, default=100, help='number of epochs with the initial learning rate')
+    parser.add_argument('--n_epochs_decay', type=int, default=100, help='number of epochs to linearly decay learning rate to zero')
+    parser.add_argument('--beta1', type=float, default=0.5, help='momentum term of adam')
+    parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate for adam')
+    parser.add_argument('--gan_mode', type=str, default='lsgan', help='the type of GAN objective. [vanilla| lsgan | wgangp]. vanilla GAN loss is the cross-entropy objective used in the original GAN paper.')
+    parser.add_argument('--pool_size', type=int, default=50, help='the size of image buffer that stores previously generated images')
+    parser.add_argument('--lr_policy', type=str, default='linear', help='learning rate policy. [linear | step | plateau | cosine]')
+    parser.add_argument('--lr_decay_iters', type=int, default=50, help='multiply by a gamma every lr_decay_iters iterations')
+
+    opt = parser.parse_args()
+
+    dataset = create_data(opt)
+    dataset_size = len(dataset)    # get the number of images in the dataset.
+    print('The number of training images = %d' % dataset_size)
+
+    model = create_model(opt)
+    total_iters = 0
+    for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):
+        epoch_start = time.time()
+        for i,data in enumerate(dataset):
+            model.set_input(data)
+            model.optimize_parameters()
+
